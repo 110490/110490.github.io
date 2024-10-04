824cf44ea8cacb19a9a4d3148b63539d5b5cb5d0
110490 <110490@stu.hgsh.hc.edu.tw>
2024-10-04 11:26:04 +0800

    Add files via upload

---
+++ hw2/script.js
@@ -0,0 +1,61 @@
+// 定義變數
+let canvas = document.getElementById("myCanvas");
+let ctx = canvas.getContext("2d");
+var randomColor = Math.floor(Math.random()*16777215).toString(16);
+let x = -100, y = -100, dx = 10, dy = 10, r = 30, aa = 0 ;
+
+
+// 按下按鍵時觸發
+document.addEventListener("mousemove", mousemove);
+function mousemove(e) 
+{
+		if(aa == 1)
+		{	
+				x = e.clientX - canvas.offsetLeft;
+				y = e.clientY - canvas.offsetTop;
+				
+		}
+            document.addEventListener("mousedown", mousedown);
+			document.addEventListener("mouseup", mouseup);		
+}
+function mousedown() 
+{
+	aa = 1;
+}
+function mouseup() 
+{
+	aa = 0;
+}
+
+  if(aa==1)
+  {  
+  const randomColor = Math.floor(Math.random()*16777215).toString(16);
+  color = "#" + randomColor;
+  }
+  else 
+  {
+	  color = "#";
+	}
+  
+
+// TODO: 滑鼠移動(mousemove)時觸發，改變位置(x, y)為滑鼠目前位置(e.clientX, e.clientY)
+// ...
+
+
+// 畫圓形
+function drawBall() 
+{
+	ctx.beginPath();
+	ctx.arc(x, y, r, 0, Math.PI*2); // arc(圓心x, 圓心y, 半徑, 起始角, 結束角)
+	ctx.fillStyle = color;
+	ctx.fill();
+	ctx.closePath();
+}
+
+// 更新畫布
+function draw() 
+{	
+    drawBall();
+    requestAnimationFrame(draw);
+}
+draw();
